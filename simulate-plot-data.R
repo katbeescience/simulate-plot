c9cf80d5f0281597342b028aa6f818c58f2aab76
Kathryn Busby <mkbusby@email.arizona.edu>
2021-03-23 10:07:09 -0700

    Initial commit with data simulation and plot

---
+++ simulate-plot-data.R
@@ -0,0 +1,13 @@
+# Simulate and plot data.
+# Kathryn Busby
+# mkbusby@email.arizona.edu
+# 2021-03-23
+
+# Simulate data from normal distribution
+
+x <- rnorm(n=100)
+y <- 2 * x + rnorm(n = 100, sd = 0.1)
+
+# Plot simulated data
+
+plot(x=x, y=y)
